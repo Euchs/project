e6c9ea23bf53f211831eb93cd3a5a6ec39be8f7a
WhyliphOmondi <whyliphomondi@gmail.com>
2018-08-15 15:59:22 +0300

    Web Content

--- InputValidator.java
+++
@@ -1,7 +0,0 @@
-package manageProjects;
-
-public class InputValidator {
-	//Plot label must be a string in caps
-	//Registration numbers must be a string starting with S13**
-	//Titles and Comments must be strings
-}
